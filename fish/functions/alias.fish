2867101759de22f9503142d54797d2c79c95685b
Jerome Gagner <jgagner@fulcrum.net>
2013-07-15 11:21:35 -0700

    added in my dotfiles

---
+++ fish/functions/alias.fish
@@ -0,0 +1,7 @@
+alias ltunnel "ssh -D 4000 198.211.115.156 -l root"
+alias .. "cd .."
+alias ... "cd ../.."
+alias ll "ls -la"
+alias less "less -rFKRX"
+alias startpg "pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
+alias stoppg "pg_ctl -D /usr/local/var/postgres stop -s -m fast"
