28d1fbdf172d036ac862ebaf752e96ce969d4306
Jerome Gagner <phpmonkey@gmail.com>
2014-01-27 19:01:54 -0800

    Fixed terlar prompt

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,8 @@ set -x RUBY_HEAP_SLOTS_INCREMENT 300000
 set -x RUBY_HEAP_SLOTS_GROWTH_FACTOR 1
 set -x RUBY_GC_MALLOC_LIMIT 79000000
 
+set -xg fish_color_user magenta
+set -xg fish_color_host yellow
 
 cd . 
 
